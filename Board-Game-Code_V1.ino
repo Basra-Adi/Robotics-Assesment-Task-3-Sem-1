23b72edefdc76fd4e4e92020f682f1a088695a76
Basra-Adi <9943359@schoolsnet.act.edu.au>
2026-05-10 23:16:29 +1000

    Update Board-Game-Code_V1.ino

--- Board-Game-Code_V1.ino
+++ Board-Game-Code_V1.ino
@@ -1,15 +1,35 @@
 const int joy_x = A1;
 const int joy_y = A0;
 const int joy_btn = 9;
+
+//Button Debounce
 int btn_state;     
 int lastButtonState = LOW;
 int btn_press = LOW; 
 unsigned long lastDebounceTime = 0; 
 unsigned long debounceDelay = 50;
+
+// Information and nav variables
 int nav = 0;
-int move_x;
-int move_y;
+char nav_sub;
 int num_players;
+
+//Joystick Debounce
+// x value deb
+int move_x;
+int x_state;
+int x_count;
+unsigned long x_deb;
+
+// y value deb
+int move_y;
+int y_state;
+int y_count;
+unsigned long y_deb;
+
+
+
+
 #include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_PCD8544.h>
@@ -27,88 +47,132 @@ void setup()   {
 
 void sys_menu() {
   if (nav == 0){
+    // display init
     display.clearDisplay();
     display.setTextSize(1);
     display.setTextColor(WHITE, BLACK);
+
+    // display info
     display.setCursor(13, 20);
     display.print("New, Game!");
     display.display();
-    if (btn_press == HIGH){
-      nav = nav + 1;
-      btn_press = LOW;
-    }
   }
   else if (nav == 1){
+    // display init
     display.clearDisplay();
     display.setTextSize(1);
     display.setTextColor(WHITE, BLACK);
+
+    //display info
     display.setCursor(0, 0);
     display.println("Number of ");
-    display.println("Players: ");
+    display.println("Players: 0");
     display.display();
-    if (btn_press == HIGH){
-      nav = nav + 1;
-      btn_press = LOW;
-    }
   }
   else if (nav == 2){
+    // display init
     display.clearDisplay();
     display.setTextSize(1);
     display.setTextColor(BLACK);
+
+    // display info
     display.setCursor(0, 0);
     display.println("Number of ");
-    display.println("Players: ");
+    display.print("Players: ");
     display.setTextColor(WHITE, BLACK);
-    if (move_y == "UP"){
-      num_players = num_players + 1;
-    }
-    if (move_y == "DOWN"){
-      num_players = num_players - 1;
-    }
-    display.print(num_players);
-    if (btn_press == HIGH){
-      nav = nav - 1;
-      btn_press = LOW;
-    }
- }
+    display.println(num_players);
+    display.display();
+
+    Serial.print(num_players);
+    Serial.print(" "); 
+  }
 }
 
 void joy_val() {
   int val_x = analogRead(joy_x);
-  Serial.print("X: ");
-  Serial.print(val_x);
-  Serial.print(" ");
-
   int val_y = analogRead(joy_y);
-  Serial.print("Y: ");
-  Serial.print(val_y);
-  Serial.print(" ");
-
   int val_btn = digitalRead(joy_btn);
-  Serial.print("btn: ");
-  Serial.print(val_btn);
 
-  if (val_x > 900) {
-    Serial.print(" RIGHT ");
+
+  // joy x val read
+  if (val_x > 700) {
     move_x = 1;
   }
   else if (val_x < 100){
-    Serial.print(" LEFT ");
+    move_x = 2;
+  }
+  else{
     move_x = 0;
   }
 
-  if (val_y > 900) {
-    Serial.print(" DOWN ");
-    move_y = 0;
-  }
-  else if (val_y < 100){
-    Serial.print(" UP ");
-    move_y = 1;
+  // joy x val deb 
+  if (millis() - x_deb > 250){
+    x_deb = millis();
+    if (x_state != move_x){
+
+      x_count = HIGH;
+      x_state = move_x;
+
+      if (x_count == HIGH){
+        if (move_x == 1){
+          nav = nav + 1;
+        }
+
+        else if (move_x == 2){
+          nav = nav - 1;
+        }
+      }
+      x_count = LOW;
+    }
+    else{
+    x_count = LOW;
+    }
   }
 
-  if (val_btn != lastButtonState) {
-    lastDebounceTime = millis();
+  Serial.print(nav);
+  Serial.print(btn_press);
+  Serial.print(val_btn);
+
+  // joy y val read
+  if (val_y > 700) {
+    move_y = 2;
   }
+  else if (val_y < 100){
+    move_y = 1;
+  }
+  else{
+    move_y = 0;
+    y_state = move_y;
+  }
+
+  // joy y val deb
+  if (millis() - y_deb > 250){
+    y_deb = millis();
+    if (y_state != move_y){
+
+      y_count = HIGH;
+      y_state = move_y;
+
+      if (y_count == HIGH){
+        if (move_y == 2){
+          num_players = num_players - 1;
+        }
+
+        else if (move_y == 1){
+          num_players = num_players + 1;
+        }
+      }
+      y_count = LOW;
+    }
+    else{
+    y_count = LOW;
+    }
+  }
+  
+  // btn deb
+  if (val_btn != lastButtonState) {
+      lastDebounceTime = millis();
+    }
 
   if ((millis() - lastDebounceTime) > debounceDelay) {
     if (val_btn != btn_state) {
